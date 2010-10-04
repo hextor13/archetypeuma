5bf504e67931a55f4953a4d4bcf0edc4089d045a
javicisneros <javicisneros@gmail.com>
2010-10-04 19:40:53 +0000

    Actualización del core

---
+++ core/src/main/java/org/archetypeUma/dao/interfaces/ICityDao.java
@@ -0,0 +1,11 @@
+package org.archetypeUma.dao.interfaces;
+
+import org.archetypeUma.model.pojos.City;
+
+/**
+ *
+ * @author jcisneros
+ */
+public interface ICityDao extends IBaseDao<City, Long> {
+
+}
---
+++ core/src/main/java/org/archetypeUma/dao/interfaces/IUserDao.java
@@ -0,0 +1,11 @@
+package org.archetypeUma.dao.interfaces;
+
+import org.archetypeUma.model.pojos.User;
+
+/**
+ *
+ * @author jcisneros
+ */
+public interface IUserDao extends IBaseDao<User, Long> {
+
+}
