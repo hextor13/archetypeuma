1b870b363a31ddf38922661df4600af8a46db385
javicisneros <javicisneros@gmail.com>
2010-10-04 19:41:03 +0000

    Actualización del core

--- core/src/main/java/org/archetypeUma/service/implementation/UserManagerImpl.java
+++ core/src/main/java/org/archetypeUma/service/implementation/UserManagerImpl.java
@@ -1,7 +1,53 @@
-package org.archetypeUma.service.implmentation;
+package org.archetypeUma.service.implementation;
 
+import java.util.List;
+
+import org.archetypeUma.dao.interfaces.ICityDao;
+import org.archetypeUma.dao.interfaces.IUserDao;
+import org.archetypeUma.model.pojos.City;
+import org.archetypeUma.model.pojos.User;
 import org.archetypeUma.service.interfaces.IUserManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+/**
+ * Service from users.
+ *
+ * @author jcisneros
+ */
+@Service(value="userManager")
 public class UserManagerImpl implements IUserManager {
 
+    @Autowired
+    @Qualifier("userDao")
+    private IUserDao userDao = null;
+
+    @Autowired
+    @Qualifier("cityDao")
+    private ICityDao cityDao = null;
+
+    /**
+     * @return
+     */
+    public List<User> getAll() {
+        return userDao.getAll(User.class);
+    }
+
+    /**
+     * @return
+     */
+    public List<City> getAllCity() {
+        return cityDao.getAll(City.class);
+    }
+
+    // Getters && Setters
+
+    public void setUserDao(IUserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void setCityDao(ICityDao cityDao) {
+        this.cityDao = cityDao;
+    }
 }
--- core/src/main/java/org/archetypeUma/service/interfaces/IUserManager.java
+++ core/src/main/java/org/archetypeUma/service/interfaces/IUserManager.java
@@ -1,5 +1,16 @@
 package org.archetypeUma.service.interfaces;
 
+import java.util.List;
+
+import org.archetypeUma.model.pojos.User;
+
+//@Transactional(readOnly = true)
 public interface IUserManager {
+
+    /**
+     *
+     * @return
+     */
+    List<User> getAll();
 
 }
