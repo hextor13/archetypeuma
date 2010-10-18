fd113c7024bc79d3f1f78f902609486bcf4fbc5e
javicisneros <javicisneros@gmail.com>
2010-10-18 14:24:43 +0000

    Actualizando el paquete web

--- web/src/main/java/org/archetypeUma/ws/interfaces/ICityWS.java
+++ web/src/main/java/org/archetypeUma/ws/interfaces/ICityWS.java
@@ -8,8 +8,6 @@ import org.archetypeUma.model.pojos.City;
 @WebService
 public interface ICityWS {
 
-    String example(String name);
-
     @WebResult(name="city")
     City getCity(@WebParam(name="city") String name);
 
